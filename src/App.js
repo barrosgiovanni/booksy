1cddf5665e3480eca1841265b49653e92d7ab5bd
Giovanni Barros <barrosgiovanni@icloud.com>
2023-02-15 15:41:13 -0300

    solved problem with searches with no result

--- src/App.js
+++ src/App.js
@@ -13,13 +13,34 @@ function App() {
     setBooks(result);
   }
 
-  return (
-    <div className='app'>
-      <SearchBar onSubmit={handleSubmit}/>
-      { books.length >= 1 ? <BookList books={books}/> : <h1>No searches were executed yet.<br></br>Please, type a search term...</h1> }
+  if (books === undefined) {
+    return (
+      <div className='app'>
+        <SearchBar onSubmit={handleSubmit} />
+        <h1>No results were found.</h1>
+      </div>
+    )
 
-    </div>
-  );
+  } else {
+    if (books.length === 0) {
+      return (
+        <div className='app'>
+          <SearchBar onSubmit={handleSubmit} />
+          <h1>No searches were executed yet.<br></br>Please, type a search term...</h1>
+        </div>
+      )
+
+    } else {
+      return (
+        <div className='app'>
+          <SearchBar onSubmit={handleSubmit} />
+          <BookList books={books} />
+        </div>
+      )
+    }
+  }
+
+
 }
 
 export default App;
