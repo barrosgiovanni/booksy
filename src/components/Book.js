634918862582b3458c0553d32cde6c86140a29c1
Giovanni Barros <barrosgiovanni@icloud.com>
2023-02-15 14:49:48 -0300

    solved problem with undefined author

--- src/components/Book.js
+++ src/components/Book.js
@@ -9,9 +9,12 @@ function Book({ title, authors, image, description, url }) {
         src={image}
         alt={title} />
       <h3>{title}</h3>
-      <h4 className='author'>{authors[0]}</h4>
-      <h4 className='author'>{authors[1]}</h4>
-      <h4 className='author'>{authors[2]}</h4>
+      <div className='authors'>
+      { authors && <h4 className='author'>{authors[0]}</h4>}
+      { authors && <h4 className='author'>{authors[1]}</h4>}
+      { authors && <h4 className='author'>{authors[2]}</h4>}
+      { authors && <h4 className='author'>{authors[3]}</h4>}
+      </div>
       <Description description={description} />
       <a href={url}><button className='btn-info'>+ More info</button></a>
     </article>
